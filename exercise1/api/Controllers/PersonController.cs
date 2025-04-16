81836a14efe65ed1260eebca5b860777cfa9615a
Zach Meyers <zach@zachmeyers.com>
2025-04-16 12:20:37 -0500

    what i have

---
+++ exercise1/api/Controllers/PersonController.cs
@@ -0,0 +1,95 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using StargateAPI.Business.Commands;
+using StargateAPI.Business.Queries;
+using System.Net;
+
+namespace StargateAPI.Controllers
+{
+   
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public PersonController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetPeople()
+        {
+            try
+            {
+                var result = await _mediator.Send(new GetPeople()
+                {
+
+                });
+                // TODO: Log GET request as an INFO level log
+                return this.GetResponse(result);
+            }
+            catch (Exception ex)
+            {
+                // TODO: Log exception as an ERROR
+                return this.GetResponse(new BaseResponse()
+                {
+                    Message = ex.Message,
+                    Success = false,
+                    ResponseCode = (int)HttpStatusCode.InternalServerError
+                });
+            }
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GetPersonByName(string name)
+        {
+            try
+            {
+                var result = await _mediator.Send(new GetPersonByName()
+                {
+                    Name = name
+                });
+                // TODO: Check that the person exists and either return or log it accordingly
+                // Maybe in the UI, if the person doesnt exist, we can ask the user if they want to create them?
+                return this.GetResponse(result);
+            }
+            catch (Exception ex)
+            {
+                // TODO: Log exception as an ERROR
+                return this.GetResponse(new BaseResponse()
+                {
+                    Message = ex.Message,
+                    Success = false,
+                    ResponseCode = (int)HttpStatusCode.InternalServerError
+                });
+            }
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> CreatePerson([FromBody] string name)
+        {
+            try
+            {
+                var result = await _mediator.Send(new CreatePerson()
+                {
+                    Name = name
+                });
+                // TODO: Log successful creation with person details
+                return this.GetResponse(result);
+            }
+            catch (Exception ex)
+            {
+                // TODO: Log exception as an ERROR
+                // Some errors that may happen here are the name already exists, or a connection issue
+                return this.GetResponse(new BaseResponse()
+                {
+                    Message = ex.Message,
+                    Success = false,
+                    ResponseCode = (int)HttpStatusCode.InternalServerError
+                });
+            }
+
+        }
+    }
+}
---
+++ exercise1/api/Tests/CreateAstronautDutyTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StargateAPI.Tests
+{
+    //[TestClass]
+    //public class CreateAstronautDutyTests
+    //{
+    //    [TestMethod]
+    //    [TestCategory("AstronautDuty")]
+
+    //}
+}
